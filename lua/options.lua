bf4bc4f3ba3d36bf633e414911000310b48b3e37
TashiKani-2 <ilfti@foxmail.com>
2024-11-21 21:06:44 +0800

    v0.0.1

---
+++ lua/options.lua
@@ -0,0 +1,11 @@
+vim.g.encoding = "UTF-8"
+vim.o.fileencoding = "utf-8"
+
+vim.wo.number = true
+vim.o.relativenumber = true
+
+vim.wo.cursorline = true
+
+vim.wo.colorcolumn = "80"
+
+vim.o.mouse = "a"
